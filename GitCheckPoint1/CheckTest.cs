23afa21d7ffaefe4db6f2e9732c6a1b77d76ad9f
AmineelMeck <amine.e.m@hotmail.se>
2023-02-16 14:35:49 +0100

    1st commit

---
+++ GitCheckPoint1/CheckTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitCheckPoint1
+{
+    public class CheckTest
+    {
+        
+        
+    }
+
+    //create a  method called testMessage with one argument that is a string and is used for output a message in the Console. Call this method from Main method in Program.cs and provide the text "Task 2 checked". Compile and run the program.
+}
--- GitCheckPoint1/Program.cs
+++ GitCheckPoint1/Program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("This is the Git checkpoint task 1.1");
+
+            TestMessage();
+            
+        }
+
+        public static string TestMessage()
+        {
+            string msg = "Task 2 checked";
+            return msg;
         }
     }
 }
